dafe2fb1188016278030f3a8525259a3edd2c1f2
Hutomo <hutomosaktikartiko.siskom@gmail.com>
2020-05-16 12:49:57 +0700

    Get data by id

--- controller.js
+++ controller.js
@@ -9,11 +9,23 @@ exports.index = function(req, res) {
 
 //manmpilkan semua data mahasiswa
 exports.tampilsemuadatamahasiswa = function(req, res) {
-        connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds) {
+        connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
             if(error) {
                 connection.log(error);
             } else {
                 response.ok(rows, res)
             }
         });
+}
+
+//menampilkan data mahasiswa berdasarkan id
+exports.tampildataberdasakanid = function(req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function(error, rows, fields) {
+        if(error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 }
