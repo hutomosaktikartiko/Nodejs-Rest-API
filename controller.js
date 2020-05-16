853d1af50bc91ca6a5446a4aba551637dc68a273
Hutomo <hutomosaktikartiko.siskom@gmail.com>
2020-05-16 12:41:28 +0700

    Get data from database

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./rest');
 var connection = require('./connect');
 
 exports.index = function(req, res) {
-    response.ok('REST API Started')
+    response.ok('REST API Started', res)
+}
+
+//manmpilkan semua data mahasiswa
+exports.tampilsemuadatamahasiswa = function(req, res) {
+        connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds) {
+            if(error) {
+                connection.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        });
 }
