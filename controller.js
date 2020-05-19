d4428ef3c30c2339d84e03e72e7df18cbcffae4d
Hutomo <hutomosaktikartiko.siskom@gmail.com>
2020-05-19 15:23:00 +0700

    Delete data

--- controller.js
+++ controller.js
@@ -70,3 +70,18 @@ exports.ubahMahasiswa = function(req, res) {
     }
     )
 } 
+
+//Hapus mahasiswa
+exports.hapusMahasiswa = function(req, res) {
+    var id = req.body.id_mahasiswa;
+
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+    function(error, rows, fields) {
+        if(error) {
+            console.log(error);
+        } else {
+            response.ok("Berhasil hapus data", res)
+        }
+    }
+    )
+}
--- routes.js
+++ routes.js
@@ -12,4 +12,6 @@ module.exports = function(app) {
     app.route('/tambah').post(jsonku.tambahMahasiswa);
 
     app.route('/ubah').put(jsonku.ubahMahasiswa);
+
+    app.route('/hapus').delete(jsonku.hapusMahasiswa);
 }
