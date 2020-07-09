eb173a1f986a08f186f5ab279ee6a2bbbadb2408
unknown <hutomosaktikartiko.siskom@gmail.com>
2020-07-10 00:35:03 +0700

    Role Done

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var auth = require('./auth');
+var verifikasi = require('./verifikasi');
 var router = express.Router();
 
 //Daftarkan menu register
 router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
+
+//alamat yang perlu otorisasi
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia);
 
 
 module.exports = router;
